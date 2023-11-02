36099f825d9e149bd8811f9c9151fc8adccbcfc4
TheCoteen <134722907+TheCoteen@users.noreply.github.com>
2023-11-02 23:02:03 +0400

    Add files via upload

---
+++ Car Race/Car Race/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Car_Race
+{
+    partial class Car_Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.enemy1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.enemy2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.scorelabel = new System.Windows.Forms.Label();
+            this.gametimer = new System.Windows.Forms.Timer(this.components);
+            this.enemy3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // enemy1
+            // 
+            this.enemy1.Image = global::Car_Race.Properties.Resources.Screenshot_2023_06_09_204743;
+            this.enemy1.Location = new System.Drawing.Point(473, 433);
+            this.enemy1.Name = "enemy1";
+            this.enemy1.Size = new System.Drawing.Size(65, 81);
+            this.enemy1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy1.TabIndex = 4;
+            this.enemy1.TabStop = false;
+            this.enemy1.Tag = "enemy";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.White;
+            this.pictureBox4.Location = new System.Drawing.Point(292, 501);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(24, 195);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "string";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(292, 247);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(24, 195);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "string";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(292, -2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(24, 195);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "string";
+            // 
+            // player
+            // 
+            this.player.Image = global::Car_Race.Properties.Resources.Screenshot_2023_06_09_204703;
+            this.player.Location = new System.Drawing.Point(221, 598);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(65, 81);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // enemy2
+            // 
+            this.enemy2.Image = global::Car_Race.Properties.Resources.Screenshot_2023_06_09_204743;
+            this.enemy2.Location = new System.Drawing.Point(62, 310);
+            this.enemy2.Name = "enemy2";
+            this.enemy2.Size = new System.Drawing.Size(65, 81);
+            this.enemy2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy2.TabIndex = 5;
+            this.enemy2.TabStop = false;
+            this.enemy2.Tag = "enemy";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::Car_Race.Properties.Resources.Screenshot_2023_06_09_205140;
+            this.pictureBox6.Location = new System.Drawing.Point(539, 229);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(44, 33);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Tag = "coin";
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::Car_Race.Properties.Resources.Screenshot_2023_06_09_205140;
+            this.pictureBox7.Location = new System.Drawing.Point(242, 295);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(44, 33);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Tag = "coin";
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::Car_Race.Properties.Resources.Screenshot_2023_06_09_205140;
+            this.pictureBox8.Location = new System.Drawing.Point(62, 82);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(44, 33);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Tag = "coin";
+            // 
+            // scorelabel
+            // 
+            this.scorelabel.BackColor = System.Drawing.SystemColors.Control;
+            this.scorelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorelabel.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.scorelabel.Location = new System.Drawing.Point(494, 9);
+            this.scorelabel.Name = "scorelabel";
+            this.scorelabel.Size = new System.Drawing.Size(137, 93);
+            this.scorelabel.TabIndex = 9;
+            this.scorelabel.Text = "Score : 0";
+            // 
+            // gametimer
+            // 
+            this.gametimer.Enabled = true;
+            this.gametimer.Interval = 20;
+            this.gametimer.Tick += new System.EventHandler(this.gametimerevent);
+            // 
+            // enemy3
+            // 
+            this.enemy3.Image = global::Car_Race.Properties.Resources.Screenshot_2023_06_09_204743;
+            this.enemy3.Location = new System.Drawing.Point(379, 131);
+            this.enemy3.Name = "enemy3";
+            this.enemy3.Size = new System.Drawing.Size(65, 81);
+            this.enemy3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy3.TabIndex = 10;
+            this.enemy3.TabStop = false;
+            this.enemy3.Tag = "enemy";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Car_Race.Properties.Resources.Screenshot_2023_06_09_204743;
+            this.pictureBox1.Location = new System.Drawing.Point(213, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 81);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "enemy";
+            // 
+            // Car_Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(633, 717);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.enemy3);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.enemy2);
+            this.Controls.Add(this.enemy1);
+            this.Controls.Add(this.scorelabel);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Car_Game";
+            this.Text = "Car Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox enemy1;
+        private System.Windows.Forms.PictureBox enemy2;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Label scorelabel;
+        private System.Windows.Forms.Timer gametimer;
+        private System.Windows.Forms.PictureBox enemy3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Car Race/Car Race/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Car_Race
+{
+    public partial class Car_Game : Form
+    {
+
+        
+
+        int turnspeed = 8;
+
+        int score = 0;
+
+        int enemyspeed = 10;
+
+        int coinspeed = 7;
+
+        int stringspeed = 8;
+
+        bool goright, goleft;
+
+        public Car_Game()
+        {
+            InitializeComponent();
+        }
+
+
+        private void gametimerevent(object sender, EventArgs e)
+        {
+            scorelabel.Text = "Score : " + score;
+
+            if(goright == true)
+            {
+                player.Left += turnspeed;
+            }
+            if(goleft == true)
+            {
+                player.Left -= turnspeed;
+            }
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox)
+                {
+                    if((string) x.Tag == "string")
+                    {
+                        x.Top += stringspeed;
+
+                        if (x.Top > 420)
+                        {
+                            x.Top = -195;
+                        }
+                    }
+
+                    if((string) x.Tag == "enemy")
+                    {
+                        x.Top += enemyspeed;
+                        
+                        if(x.Top > 760)
+                        {
+                            x.Top = 0;
+                        }
+
+                        if (player.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            endgame();
+                        }
+                     
+                    }
+                    if((string)x.Tag == "coin")
+                    {
+                        x.Top += coinspeed;
+
+                        if (player.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            x.Visible = false;
+                            score++;
+                        }
+                        
+
+                        if(x.Top > 797 || x.Visible == false)
+                        {
+                            x.Top = 0;
+                            x.Visible = true;
+                        }
+                    }
+                }
+            }
+
+
+        }
+
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+
+            if(e.KeyCode == Keys.Right)
+            {
+                goright = true;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = false;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false;
+            }
+        }
+
+        private void endgame()
+        {
+            gametimer.Stop();
+
+            scorelabel.Text += "\nGame over. You lost!!!!";
+        }
+
+        //private void restart()
+
+        
+    }
+}
