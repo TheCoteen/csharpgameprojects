d336236d8934d9e9e2f2f77371a839def954d821
TheCoteen <134722907+TheCoteen@users.noreply.github.com>
2023-11-11 20:28:20 +0400

    Add files via upload

---
+++ MultiPlayerInvader/MultiPlayerInvader/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace MultiPlayerInvader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bulletright = new System.Windows.Forms.PictureBox();
+            this.playerright = new System.Windows.Forms.PictureBox();
+            this.playerleft = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bulletleft = new System.Windows.Forms.PictureBox();
+            this.labelright = new System.Windows.Forms.Label();
+            this.labelleft = new System.Windows.Forms.Label();
+            this.gametimer = new System.Windows.Forms.Timer(this.components);
+            this.resultlabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bulletright)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerright)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerleft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bulletleft)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bulletright
+            // 
+            this.bulletright.Image = global::MultiPlayerInvader.Properties.Resources.bulletl;
+            this.bulletright.Location = new System.Drawing.Point(867, 320);
+            this.bulletright.Name = "bulletright";
+            this.bulletright.Size = new System.Drawing.Size(52, 30);
+            this.bulletright.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bulletright.TabIndex = 4;
+            this.bulletright.TabStop = false;
+            this.bulletright.Tag = "bullet";
+            // 
+            // playerright
+            // 
+            this.playerright.Image = global::MultiPlayerInvader.Properties.Resources.spyelow;
+            this.playerright.Location = new System.Drawing.Point(842, 297);
+            this.playerright.Name = "playerright";
+            this.playerright.Size = new System.Drawing.Size(100, 90);
+            this.playerright.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.playerright.TabIndex = 2;
+            this.playerright.TabStop = false;
+            this.playerright.Tag = "player";
+            // 
+            // playerleft
+            // 
+            this.playerleft.Image = global::MultiPlayerInvader.Properties.Resources.spred1;
+            this.playerleft.Location = new System.Drawing.Point(23, 297);
+            this.playerleft.Name = "playerleft";
+            this.playerleft.Size = new System.Drawing.Size(100, 96);
+            this.playerleft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.playerleft.TabIndex = 1;
+            this.playerleft.TabStop = false;
+            this.playerleft.Tag = "player";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(489, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(11, 684);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bulletleft
+            // 
+            this.bulletleft.Image = global::MultiPlayerInvader.Properties.Resources.bulletr1;
+            this.bulletleft.Location = new System.Drawing.Point(47, 320);
+            this.bulletleft.Name = "bulletleft";
+            this.bulletleft.Size = new System.Drawing.Size(45, 35);
+            this.bulletleft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bulletleft.TabIndex = 3;
+            this.bulletleft.TabStop = false;
+            this.bulletleft.Tag = "bullet";
+            // 
+            // labelright
+            // 
+            this.labelright.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelright.Location = new System.Drawing.Point(822, 0);
+            this.labelright.Name = "labelright";
+            this.labelright.Size = new System.Drawing.Size(152, 57);
+            this.labelright.TabIndex = 5;
+            this.labelright.Text = "Heart : 10";
+            // 
+            // labelleft
+            // 
+            this.labelleft.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelleft.Location = new System.Drawing.Point(1, 0);
+            this.labelleft.Name = "labelleft";
+            this.labelleft.Size = new System.Drawing.Size(152, 57);
+            this.labelleft.TabIndex = 6;
+            this.labelleft.Text = "Heart : 10";
+            // 
+            // gametimer
+            // 
+            this.gametimer.Enabled = true;
+            this.gametimer.Interval = 20;
+            this.gametimer.Tick += new System.EventHandler(this.gametimerevent);
+            // 
+            // resultlabel
+            // 
+            this.resultlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultlabel.Location = new System.Drawing.Point(357, 0);
+            this.resultlabel.Name = "resultlabel";
+            this.resultlabel.Size = new System.Drawing.Size(278, 103);
+            this.resultlabel.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(986, 683);
+            this.Controls.Add(this.labelleft);
+            this.Controls.Add(this.labelright);
+            this.Controls.Add(this.playerright);
+            this.Controls.Add(this.playerleft);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.bulletleft);
+            this.Controls.Add(this.bulletright);
+            this.Controls.Add(this.resultlabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.bulletright)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerright)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerleft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bulletleft)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox playerleft;
+        private System.Windows.Forms.PictureBox playerright;
+        private System.Windows.Forms.PictureBox bulletleft;
+        private System.Windows.Forms.PictureBox bulletright;
+        private System.Windows.Forms.Label labelright;
+        private System.Windows.Forms.Label labelleft;
+        private System.Windows.Forms.Timer gametimer;
+        private System.Windows.Forms.Label resultlabel;
+    }
+}
+
---
+++ MultiPlayerInvader/MultiPlayerInvader/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiPlayerInvader
+{
+    public partial class Form1 : Form
+    {
+
+        int scoreright = 10, scoreleft = 10;
+
+        int playerspeed = 8;
+
+        int bulletspeedleft, bulletspeedright;
+
+        bool goupright, goupleft;
+
+        bool godownright, godownleft;
+
+        bool gorightright, gorightleft;
+
+        bool goleftleft, goleftright;
+
+        bool shootingleft, shootingright;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gametimerevent(object sender, EventArgs e)
+        {
+            labelright.Text = "Heart : " + scoreright;
+            labelleft.Text = "Heart : " + scoreleft;
+
+            if(scoreleft == 0)
+            {
+                resultlabel.Text = "Player on the right wins!!!!";
+                endgame();
+            }
+
+            if(scoreright == 0)
+            {
+                resultlabel.Text = "Player on the left wins!!!!";
+                endgame();
+            }
+
+            if(goleftleft == true && playerleft.Left>0)
+            {
+                playerleft.Left -= playerspeed;
+            }
+
+            if (gorightleft == true && playerleft.Left < 300)
+            {
+                playerleft.Left += playerspeed;
+            }
+
+            if (goupleft == true && playerleft.Top > 0)
+            {
+                playerleft.Top -= playerspeed;
+            }
+
+            if (godownleft == true && playerleft.Top < 300)
+            {
+                playerleft.Top += playerspeed;
+            }
+
+            if (goleftright == true && playerright.Left > 400)
+            {
+                playerright.Left -= playerspeed;
+            }
+
+            if (gorightright == true && playerright.Left < 900)
+            {
+                playerright.Left += playerspeed;
+            }
+
+            if (goupright == true && playerright.Top > 0)
+            {
+                playerright.Top -= playerspeed;
+            }
+
+            if (godownright == true && playerright.Top < 600)
+            {
+                playerright.Top += playerspeed;
+            }
+
+
+            if (shootingleft == true)
+            {
+                bulletspeedleft = 40;
+                bulletleft.Left += bulletspeedleft;
+
+            }
+            else
+            {
+                bulletspeedleft = 0;
+
+                bulletleft.Left = -50;
+            }
+
+            if(bulletleft.Left > 700)
+            {
+                shootingleft = false;
+            }
+
+            if(bulletright.Left < -50)
+            {
+                shootingright = false;
+            }
+
+            if(shootingright == true)
+            {
+                bulletspeedright = 40;
+                bulletright.Left -= bulletspeedright;
+            }
+            else
+            {
+                bulletspeedright = 0;
+
+                bulletright.Left = -50;
+            }
+
+            if (bulletleft.Bounds.IntersectsWith(playerright.Bounds))
+            {
+                shootingleft = false;
+
+                scoreright--;
+            }
+            if (bulletright.Bounds.IntersectsWith(playerleft.Bounds))
+            {
+                shootingright = false;
+
+                scoreleft--;
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                goleftright = true;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                gorightright = true;
+            }
+
+            if(e.KeyCode == Keys.Up)
+            {
+                goupright = true;
+            }
+            if(e.KeyCode == Keys.Down)
+            {
+                godownright = true;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                goleftleft = true;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                gorightleft = true;
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                goupleft = true;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                godownleft = true;
+            }
+
+            if(e.KeyCode == Keys.Space && shootingleft == false)
+            {
+                shootingleft = true;
+
+                bulletleft.Top = playerleft.Top +20;
+
+                bulletleft.Left = playerleft.Left + (playerleft.Width / 2);
+
+            }
+
+            if(e.KeyCode == Keys.P && shootingright == false)
+            {
+                shootingright = true;
+
+                bulletright.Top = playerright.Top + 20;
+
+                bulletright.Left = playerright.Left + playerright.Width/2;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleftright = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                gorightright = false;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                goupright = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                godownright = false;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                goleftleft = false;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                gorightleft = false;
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                goupleft = false;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                godownleft = false;
+            }
+
+        }
+
+        private void endgame()
+        {
+
+            gametimer.Stop();
+
+        }
+
+
+
+    }
+}
